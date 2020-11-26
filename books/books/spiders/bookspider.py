5ae7a64940d3828b41ad7db51fc73a3e0714f20f
cricard7 <chad_ricard@hotmail.com>
2020-11-26 15:09:03 -0600

    full first example

--- books/books/spiders/bookspider.py
+++ books/books/spiders/bookspider.py
@@ -15,10 +15,22 @@ class BookspiderSpider(scrapy.Spider):
        for book in all_the_books:
            #each book is the article selected above
            #in each article grab the h3 anchor tag title attribute
-           title = book.xpath('.//h3/a/@title').extract_first()
-           print(title)
            #whole html xpath: response.xpath('//....
            #for on in the loop: response.xpath('.//....
-
+           title = book.xpath('.//h3/a/@title').extract_first()
+           price = book.xpath('.//div[@class="product_price"]/p[@class="price_color"]/text()').extract_first()
+           thumb = self.start_urls[0] + book.xpath('.//div[@class="image_container"]/a/img/@src').extract_first()
+           bookLink = self.start_urls[0] + book.xpath('.//div[@class="image_container"]/a/@href').extract_first()
+           print(title)
+           print(price)
+           print(thumb)
+           print(bookLink)
+            
+           yield{
+                'Title': title,
+                'Price': price,
+                'Thumb': thumb,
+                'Link': bookLink
+            }
        print(len(all_the_books))
         #pass
